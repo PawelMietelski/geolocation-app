1cbc68f3089da89d6782283bc0798e2691bbc26e
PawelMietelski <pawel.mietelski@gmail.com>
2023-11-28 16:07:15 +0100

    working functionality

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
+# frozen_string_literal: true
+
 class ApplicationController < ActionController::API
+  include ErrorHandler
+
+  def authorize_request
+    result = Auth::Decoder.call(request:)
+    return if result.success?
+    raise GeneralApiExceptions::UnauthorizedError
+  end
 end
